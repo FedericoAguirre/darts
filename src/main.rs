32ff283cfae8a24b2f04668037c496303989fc4a
Federico Aguirre <federico.aguirre.cardiel@gmail.com>
2024-01-01 17:35:32 -0600

    Score 0 integration test added

--- src/main.rs
+++ src/main.rs
@@ -136,5 +136,16 @@ mod tests {
 
         let stdout_str = String::from_utf8(output.stdout).expect("Invalid UTF-8");
         assert!(stdout_str.contains("The score for (4, -4) is: 1"));
+
+        // (-11, -11) test, score 0)
+        let output = Command::new("target/debug/darts")
+            .args(&["--x_coord", "-11.0", "--y_coord", "-11.0"])
+            .output()
+            .expect("Failed to execute process");
+
+        assert!(output.status.success());
+
+        let stdout_str = String::from_utf8(output.stdout).expect("Invalid UTF-8");
+        assert!(stdout_str.contains("The score for (-11, -11) is: 0"));
     }
 }
